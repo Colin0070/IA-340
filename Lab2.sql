065060fff0d0e0ffc0ced3f1721b30e3ae8ed18f
Colin0070 <122939417+Colin0070@users.noreply.github.com>
2023-09-06 18:03:40 -0400

    Update Lab2.sql

--- Lab2.sql
+++ Lab2.sql
@@ -1,9 +1,13 @@
 -- Create schemas
 
 -- Create tables
+
 CREATE TABLE IF NOT EXISTS professor
 (
-    
+    p_email VARCHAR(50) NOT NULL,
+    p_name VARCHAR(50) NOT NULL,
+    office VARCHAR(20) NOT NULL UNIQUE,
+    PRIMARY KEY(p_email)
 );
 
 CREATE TABLE IF NOT EXISTS student
@@ -30,13 +34,7 @@ CREATE TABLE IF NOT EXISTS enroll
     PRIMARY KEY(c_number, s_email)
 );
 
-CREATE TABLE IF NOT EXISTS professor
-(
-    p_email VARCHAR(50) NOT NULL,
-    p_name VARCHAR(50) NOT NULL,
-    office VARCHAR(20) NOT NULL UNIQUE,
-    PRIMARY KEY(p_email)
-);
+
 
 
 -- Create FKs
