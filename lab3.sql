2f002dd458e870271a7b8ff054f10033de8cb44a
Colin0070 <122939417+Colin0070@users.noreply.github.com>
2023-09-11 18:01:25 -0400

    Create lab3.sql

---
+++ lab3.sql
@@ -0,0 +1,19 @@
+-- insert into student(s_email,s_name,major)
+-- values ('s1@jmu.edu','s1','IA')
+
+-- select * from student
+
+-- update student
+-- set major = 'ISAT'
+-- where s_email = 'sl@jmu.edu'
+
+-- delete from student
+-- where s_email = 's1@jmu.edu'
+
+-- insert into student (s_email,s_name,major)
+-- values ('s1@jmu.edu','s1','IA'),
+-- 		('s2@jmu.edu','s2','IA'),
+-- 		('s3@jmu.edu','s3','ISAT'),
+-- 		('s4@jmu.edu','s4','ISAT')
+
+-- select * from student
